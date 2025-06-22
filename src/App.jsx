235b5e8c7058e1144a1cf36019159250fceac489
Cameron Stuart <cstuart08@gmail.com>
2025-06-21 21:56:46 -0400

    adds navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from "./Root.jsx";
+import Home from "./pages/Home.jsx"
 import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
     </Route>
   )
 );
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import NavigationBar from "./components/NavigationBar.jsx";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
+    <>
+      <NavigationBar />
       <Outlet />
+    </>
   );
 };
 
